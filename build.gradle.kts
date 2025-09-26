67f6db87323bde956b71543cf63a2e2fde6ee9cf
Milo <milo.gianinazzi@gmail.com>
2025-09-26 21:08:55 +0200

    replace detekt by gradle-detekt + updated yaml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     id("com.android.application") version libs.versions.agp.get() apply false
     id("org.jetbrains.kotlin.android") version libs.versions.kotlin.get() apply false
     id("org.jetbrains.kotlin.plugin.compose") version libs.versions.kotlin.get() apply false
-    id("io.gitlab.arturbosch.detekt") version libs.versions.detekt.get() apply false
+    id("dev.detekt") version(libs.versions.detekt)
 }
